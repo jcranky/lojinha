5dbe4b2f171ba3012f54eaa02ef59770eee00220
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-05-17 00:11:05 -0300

    added actors to trigger sending the item images to amazon s3 the actual sending logic is still missing

--- app/models/image-actors.scala
+++ app/models/image-actors.scala
@@ -11,13 +11,16 @@ import play.api.libs.concurrent.Akka
 import scala.util.Random
 
 //TODO: validate the image and return an error if its contentType isn't from an image
-//TODO: redimension the images to a proper size, or make uploading big images illegal?
-//TODO: send files to S3 keep the file bucket + key in the database?
+//TODO: redimension the images to a proper size, or make uploading really big images illegal?
 
 object Images {
   
   val thumberRouter =
     Akka.system.actorOf(Props[ImageThumberActor].withRouter(SmallestMailboxRouter(5)), "thumber-router")
+  val s3SenderRouter =
+    Akka.system.actorOf(Props[S3SenderActor].withRouter(SmallestMailboxRouter(2)), "s3-sender-router")
+  
+  println("PATH = " + s3SenderRouter.path)
   
   /**
    * Generates a key for the image and returns it immediatelly, while sending the
@@ -38,19 +41,39 @@ object Images {
   }
 }
 
+// image thumb stuff
 class ImageThumberActor extends Actor {
   def receive = {
-    case GenThumb(image: File) =>
+    case GenThumb(image) =>
       new ImageThumber(image).generateThumbs
+      Akka.system.actorFor("akka://application/user/s3-sender-router") ! SendToS3(image)  // actually, repeat this for each generated thumb
   }
 }
 
 class ImageThumber(image: File) {
   def generateThumbs() = {
-    println("thumb generation logic called")
+    println("thumb generation logic called - " + image.getName)
 //    val newFile = new File("/tmp/" + System.currentTimeMillis + file.filename)
 //    file.ref.moveTo(newFile)
   }
 }
 
 case class GenThumb(image: File)
+
+
+// s3 sending stuff
+
+class S3SenderActor extends Actor {
+  def receive = {
+    case SendToS3(image) =>
+      new S3Sender(image).send
+  }
+}
+
+class S3Sender(image: File) {
+  def send() = {
+    println("sending to s3..... or so you think =p " + image.getName)
+  }
+}
+
+case class SendToS3(image: File)
