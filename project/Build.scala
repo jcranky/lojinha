16faa5fa95ef1c878271849e925b0e8b1eabe7f8
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-05-16 22:03:28 -0300

    first version

---
+++ project/Build.scala
@@ -0,0 +1,18 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "lojinha"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    val appDependencies = Seq(
+      "org.mockito" % "mockito-core" % "1.9.0" % "test"
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      // Add your own project settings here      
+    )
+
+}
