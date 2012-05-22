5535ba3e10fae08317f2f58e521b839275ece0bb
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-05-22 11:32:32 -0300

    added play-salat

--- project/Build.scala
+++ project/Build.scala
@@ -8,10 +8,12 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.mockito" % "mockito-core" % "1.9.0" % "test"
+    "org.mockito" % "mockito-core" % "1.9.0" % "test",
+    "se.radley" %% "play-plugins-salat" % "1.0.3"
   )
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    // Add your own project settings here      
+    routesImport += "se.radley.plugin.salat.Binders._",
+    templatesImport += "org.bson.types.ObjectId"
   )
 }
