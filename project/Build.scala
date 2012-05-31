1f862af9b63037f350770379d56172378d82824a
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-05-31 00:46:06 -0300

    added aws lib in the dependencies list

--- project/Build.scala
+++ project/Build.scala
@@ -8,8 +8,9 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.mockito" % "mockito-core" % "1.9.0" % "test",
-    "se.radley" %% "play-plugins-salat" % "1.0.3"
+    "com.amazonaws" % "aws-java-sdk" % "1.3.10",
+    "se.radley" %% "play-plugins-salat" % "1.0.3",
+    "org.mockito" % "mockito-core" % "1.9.0" % "test"
   )
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
