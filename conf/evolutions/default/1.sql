361c1751721aec4c5f4b8c4636e3de703de8e4f4
jcranky <paulo.siqueira@gmail.com>
2012-09-13 16:46:21 -0300

    changed number to numeric, as its the way postgres needs it

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -29,7 +29,7 @@ CREATE SEQUENCE bid_id_seq;
 CREATE TABLE bid (
     id INTEGER NOT NULL DEFAULT nextval('bid_id_seq'),
     bidder_email varchar(255) NOT NULL,
-    value NUMBER(8,2),
+    value NUMERIC(8,2),
     dateTime TIMESTAMP NOT NULL,
     item_id INTEGER NOT NULL
 );
