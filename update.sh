ef248126b23b9372c836414d4ff646cbb017add7
jcranky <paulo.siqueira@gmail.com>
2012-09-16 18:08:48 -0300

    now also running the tests

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 git pull
-play clean compile stage
+play clean compile test stage
 sudo env PATH=$PATH play stop
 sudo -b ./target/start -Dhttp.port=80 -DapplyEvolutions.default=true
