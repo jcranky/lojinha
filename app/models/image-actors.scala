b3bd84e7b6e79123771bbbba47d639d7ffae0bfb
jcranky <paulo.siqueira@gmail.com>
2012-09-16 12:00:52 -0300

    added link to show the original-sized image of the product

--- app/models/image-actors.scala
+++ app/models/image-actors.scala
@@ -17,7 +17,7 @@ class Images(system: ActorSystem) {
     system.actorOf(Props[ImageThumberActor].withRouter(SmallestMailboxRouter(5)), "thumber-router")
   val s3SenderRouter =
     system.actorOf(Props[S3SenderActor].withRouter(SmallestMailboxRouter(2)), "s3-sender-router")
-  
+
   /**
    * Generates a key for the image and returns it immediatelly, while sending the
    * image to be processed asynchronously with akka.
@@ -26,13 +26,13 @@ class Images(system: ActorSystem) {
     val validChars = "abcdefghijklmnopqwxyv_"
     val imageKey = (1 to 20).foldLeft("")((t, a) => t + validChars(Random.nextInt(validChars.length)))
     thumberRouter ! GenThumb(image, imageKey)
-    
+
     imageKey
   }
-  
+
   def generateUrl(imageKey: String, thumbSize: ThumbSize): String = {
     val bucket = current.configuration.getString("aws.s3.bucket").get
-    "https://s3.amazonaws.com/%s/%s-%s.png".format(bucket, imageKey, thumbSize.suffix)
+    "https://s3.amazonaws.com/%s/%s%s.png".format(bucket, imageKey, thumbSize.suffix)
   }
 }
 
@@ -40,14 +40,14 @@ class ImageThumberActor extends Actor with ActorLogging {
   def receive = {
     case GenThumb(image, imageKey) =>
       log.info("about to generate thumbs for key {}", imageKey)
-      
+
       val images = new ImageThumber(image, imageKey).generateThumbs
       val s3SenderRouter = context.system.actorFor("akka://application/user/s3-sender-router")
-      
+
       s3SenderRouter ! SendToS3(image, imageKey + ".png")
       images foreach { imageTuple =>
         val (imageFile, imageName) = imageTuple
-        s3SenderRouter ! SendToS3(imageFile, imageName) 
+        s3SenderRouter ! SendToS3(imageFile, imageName)
       }
   }
 }
--- app/models/images/ImageHandling.scala
+++ app/models/images/ImageHandling.scala
@@ -7,14 +7,14 @@ import javax.imageio.ImageIO
 
 class ImageThumber(image: File, imageKey: String) {
   def generateThumbs(): List[(File, String)] = ImageThumber.sizes map generateThumb
-  
+
   def generateThumb(thumbSize: ThumbSize): (File, String) = {
     val imageBuf = ImageIO.read(image)
     val height = imageBuf.getHeight
     val width = imageBuf.getWidth
-    
+
     val imageName = imageKey + "-" + thumbSize.suffix + ".png"
-    
+
     if (width <= thumbSize.width && height <= thumbSize.height)
       (image, imageName)
     else {
@@ -22,14 +22,14 @@ class ImageThumber(image: File, imageKey: String) {
       (writeImage(newWidth, newHeight, imageBuf, thumbSize, imageName), imageName)
     }
   }
-  
+
   private def writeImage(width: Int, height: Int, imageBuf: BufferedImage, thumbSize: ThumbSize, imageName: String) = {
     val scaledImage = new BufferedImage(width, height,  BufferedImage.TYPE_INT_RGB)
     val g = scaledImage.createGraphics
     g.setComposite(AlphaComposite.Src)
     g.drawImage(imageBuf, 0, 0, width, height, null);
     g.dispose
-      
+
     val destFile = new File(image.getParentFile, imageName)
     ImageIO.write(scaledImage, "png", destFile)
     destFile
@@ -38,11 +38,11 @@ class ImageThumber(image: File, imageKey: String) {
 
 object ImageThumber {
   val sizes = List(SmallThumb, MediumThumb, LargeThumb)
-  
+
   def newSizesFor(thumbSize: ThumbSize, originalWidth: Int, originalHeight: Int): (Int, Int) = {
     var newWidth: Double = originalWidth
     var newHeight: Double = originalHeight
-    
+
     if (newWidth > thumbSize.width){
       newWidth = thumbSize.width
       newHeight = originalHeight * newWidth / originalWidth
@@ -52,12 +52,13 @@ object ImageThumber {
       newHeight = thumbSize.height
       newWidth = newHeight * newWidth / oldHeight
     }
-    
+
     (newWidth.toInt, newHeight.toInt)
   }
 }
 
 sealed case class ThumbSize(width: Int, height: Int, suffix: String)
-object SmallThumb extends ThumbSize(100, 100, "small")
-object MediumThumb extends ThumbSize(200, 200, "medium")
-object LargeThumb extends ThumbSize(300, 300, "large")
+object SmallThumb extends ThumbSize(100, 100, "-small")
+object MediumThumb extends ThumbSize(200, 200, "-medium")
+object LargeThumb extends ThumbSize(300, 300, "-large")
+object OriginalSize extends ThumbSize(0, 0, "")
