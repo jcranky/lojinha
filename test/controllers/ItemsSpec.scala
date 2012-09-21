2a5ca9dee23648976b891a825b8360f76358cd45
jcranky <paulo.siqueira@gmail.com>
2012-09-21 16:35:49 -0300

    simplified DAO usage

--- test/controllers/ItemsSpec.scala
+++ test/controllers/ItemsSpec.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import models.dao._
+import models.dao.DAOFactory._
 import org.specs2.mutable.Specification
 import play.api.test._
 import play.api.test.Helpers._
@@ -9,8 +9,6 @@ class ItemsSpec extends Specification {
   "the Items controller" should {
     "not have the mark as sold button for not logged in users" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        val itemDAO = DAOFactory.itemDAO
-        val categoryDAO = DAOFactory.categoryDAO
         itemDAO.create("effective java", "book description", None, categoryDAO.getByName("Books"))
 
         val Some(result) = routeAndCall(FakeRequest(GET, "/items/1"))
