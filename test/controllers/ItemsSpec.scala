bd405552cd2fe0aabb385688f70e729cd18f4104
jcranky <paulo.siqueira@gmail.com>
2012-09-21 12:15:39 -0300

    added test to verify that the soldButton is not displayed to logged out users

---
+++ test/controllers/ItemsSpec.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import models.dao._
+import org.specs2.mutable.Specification
+import play.api.test._
+import play.api.test.Helpers._
+
+class ItemsSpec extends Specification {
+  "the Items controller" should {
+    "not have the mark as sold button for not logged in users" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val itemDAO = DAOFactory.itemDAO
+        val categoryDAO = DAOFactory.categoryDAO
+        itemDAO.create("effective java", "book description", None, categoryDAO.getByName("Books"))
+
+        val Some(result) = routeAndCall(FakeRequest(GET, "/items/1"))
+
+        status(result) must equalTo(OK)
+        contentAsString(result) must not contain("soldButton")
+      }
+    }
+  }
+}
