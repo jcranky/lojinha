067aaff7089c0b199a9b9ae40ca3f6ad799545c2
jcranky <paulo.siqueira@gmail.com>
2012-11-03 21:31:29 -0200

    removed test from the update script

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 git pull
-play clean compile test stage
+play clean compile stage
 sudo env PATH=$PATH play stop
 sudo -b env PATH=$PATH ./target/start -Dhttp.port=80 -DapplyEvolutions.default=true
