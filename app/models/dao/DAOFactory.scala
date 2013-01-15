58f6658199ac78ba1618ac55f0f9b2c6de783cbc
jcranky <paulo.siqueira@gmail.com>
2013-01-14 22:55:26 -0200

    giving up mongo support\ncloses #5

--- app/models/dao/DAOFactory.scala
+++ app/models/dao/DAOFactory.scala
@@ -1,29 +1,11 @@
 package models.dao
 
 object DAOFactory {
-  val dbConfig = play.api.Play.current.configuration.getString("lojinha.db.type", Some(Set("sql", "mongo")))
+  def categoryDAO: CategoryDAO =  AnormCategoryDAO
 
-  def categoryDAO: CategoryDAO = dbConfig match {
-    case Some("mongo") => throw new UnsupportedOperationException("not implemented yet") //MongoCategoryDAO
-    case Some("sql") => AnormCategoryDAO
-    case None => AnormCategoryDAO
-  }
+  def itemDAO: ItemDAO = AnormItemDAO
 
-  def itemDAO: ItemDAO = dbConfig match {
-    case Some("mongo") => MongoItemDAO
-    case Some("sql") => AnormItemDAO
-    case None => AnormItemDAO
-  }
+  def bidDAO: BidDAO = AnormBidDAO
 
-  def bidDAO: BidDAO = dbConfig match {
-    case Some("mongo") => MongoBidDAO
-    case Some("sql") => AnormBidDAO
-    case None => AnormBidDAO
-  }
-
-  def userDAO: UserDAO = dbConfig match {
-    case Some("mongo") => throw new UnsupportedOperationException("not implemented yet") //MongoUserDAO
-    case Some("sql") => AnormUserDAO
-    case None => AnormUserDAO
-  }
+  def userDAO: UserDAO = AnormUserDAO
 }
--- project/Build.scala
+++ project/Build.scala
@@ -9,15 +9,12 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     "com.amazonaws" % "aws-java-sdk" % "1.3.14",
-    "se.radley" %% "play-plugins-salat" % "1.0.9",
     "postgresql" % "postgresql" % "9.1-901.jdbc4" % "runtime",
     "org.mockito" % "mockito-core" % "1.9.0" % "test"
   ) map (_.withSources)
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    routesImport += "se.radley.plugin.salat.Binders._",
     templatesImport ++= Seq(
-      "org.bson.types.ObjectId",
       "models.dao._"
     )
   )
