3a74718650c142a3b894e9e2f9ecaa3c6301d082
jcranky <paulo.siqueira@gmail.com>
2013-01-24 18:03:22 -0200

    now using cache to avoid re-generating the feed all the time

--- app/controllers/Feeds.scala
+++ app/controllers/Feeds.scala
@@ -1,6 +1,10 @@
 package controllers
 
+import play.api.Play.current
+import play.api.cache.Cache
 import play.api.mvc._
+import scala.xml.NodeSeq
+
 import models._
 import models.dao._
 
@@ -8,6 +12,10 @@ object Feeds extends Controller {
   val feedGen = new FeedGenerator(DAOFactory.itemDAO)
 
   def latest = Action {
-    Ok(feedGen.allItemsFeed)
+    val feedXml = Cache.getOrElse[NodeSeq]("allItems.feed") {
+      feedGen.allItemsFeed
+    }
+
+    Ok(feedXml)
   }
 }
