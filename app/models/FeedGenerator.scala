165467652532304ac5e3eb59813ee0a616cfb710
jcranky <paulo.siqueira@gmail.com>
2013-01-24 20:14:22 -0200

    first version of the feed generation

--- app/models/FeedGenerator.scala
+++ app/models/FeedGenerator.scala
@@ -1,8 +1,33 @@
 package models
 
 import models.dao.ItemDAO
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
 import scala.xml.NodeSeq
 
 class FeedGenerator(itemDAO: ItemDAO) {
-  def allItemsFeed: NodeSeq = <feed>feed!</feed>
+
+  def allItemsFeed(baseURL: String): NodeSeq = {
+    val items = itemDAO.all(false)
+
+    <feed xmlns="http://www.w3.org/2005/Atom">
+      <title>Feed da Lojinha</title>
+      <link href={baseURL}/>
+      <updated>{new DateTime().toString(ISODateTimeFormat.dateTime())}</updated>
+      <author>
+        <name>jcranky</name>
+      </author>
+      <id>{baseURL + "/feed"}</id>
+
+      {items.map { item =>
+          <entry>
+            <title>{item.name}</title>
+            <link href={baseURL + "/items/1"}/>
+            <id>{baseURL + "/items/1"}</id>
+            <updated>2013-01-24T17:13:21Z</updated>
+            <summary>{item.description}</summary>
+          </entry>
+        }}
+    </feed>
+  }
 }
---
+++ test/models/FeedGeneratorSpec.scala
@@ -0,0 +1,29 @@
+package models
+
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+
+import models.dao._
+import org.specs2.specification.Scope
+
+class FeedGeneratorSpec extends Specification with Mockito {
+  "the feed generator" should {
+    "generate a feed with the right amount of entries" in new FeedScope {
+      val feed = feedGen.allItemsFeed("http://localhost:9000")
+      (feed \\ "entry").size must_== 2
+      (feed \\ "entry" \ "title")(0).text must_== "Zelda 64"
+    }
+  }
+
+  trait FeedScope extends Scope with Mockito {
+    val cat = Category(1, "Games", "games")
+
+    val itemDAO = mock[ItemDAO]
+    itemDAO.all(false) returns List(
+      Item(1, "Zelda 64", "The Legend of Zelda 64", None, cat),
+      Item(2, "Mario 64", "Super Mario 64", None, cat)
+    )
+
+    val feedGen = new FeedGenerator(itemDAO)
+  }
+}
