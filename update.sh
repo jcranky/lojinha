ae42dcf70f714de890653dcb8419ff5c9033fbe6
jcranky <paulo.siqueira@gmail.com>
2013-05-23 20:40:28 -0300

    not running lojinha on port 80 anymore

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 git pull
 play clean compile stage
 sudo env PATH=$PATH play stop
-sudo -b env PATH=$PATH ./target/start -Dhttp.port=80 -DapplyEvolutions.default=true
+sudo -b env PATH=$PATH ./target/start -DapplyEvolutions.default=true
